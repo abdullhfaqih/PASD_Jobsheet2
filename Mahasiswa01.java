5dcf7bdc6c287210e88980e4addf6c07d31e079c
abdullah faqih <faqih7665@gmail.com>
2026-02-19 22:49:38 +0700

    Percobaan Pertama

---
+++ Mahasiswa01.java
@@ -0,0 +1,38 @@
+public class Mahasiswa01 {
+  String nim;
+  String nama;
+  String kelas;
+  double ipk;
+
+  void tampilkanInfo() {
+    System.out.println("NIM: " + nim);
+    System.out.println("Nama: " + nama);
+    System.out.println("Kelas: " + kelas);
+    System.out.println("IPK: " + ipk);
+  }
+
+  void ubahKelas(String kelasBaru) {
+    kelas = kelasBaru;
+  }
+
+  void updateIPK(double ipkBaru) {
+    if (ipkBaru <= 0.0 || ipkBaru >= 4.0) {
+      System.out.println("IPK tidak valid");
+      return;
+    }
+
+    ipk = ipkBaru;
+  }
+
+  String nilaiKinerja(double ipk) {
+    if (ipk >= 3.5) {
+      return "Kinerja Sangat Baik";
+    } else if (ipk >= 3.0) {
+      return "Kinerja Baik";
+    } else if (ipk >= 2.0) {
+      return "Kinerja Cukup";
+    } else {
+      return "Kinerja Kurang";
+    }
+  }
+}
